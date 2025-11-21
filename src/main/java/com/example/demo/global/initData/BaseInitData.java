c8dad09560161af6f61b50ab1080673fa3029ada
chacha86 <taejincha0619@gmail.com>
2025-11-21 20:44:43 +0900

    work

--- src/main/java/com/example/demo/global/initData/BaseInitData.java
+++ src/main/java/com/example/demo/global/initData/BaseInitData.java
@@ -1,12 +1,18 @@
 package com.example.demo.global.initData;
 
+import com.example.demo.domain.post.post.Post;
+import com.example.demo.domain.post.post.PostRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@RequiredArgsConstructor
 public class BaseInitData {
+
+    private final PostRepository postRepository;
 
     @Bean
     public ApplicationRunner test() {
@@ -14,7 +20,24 @@ public class BaseInitData {
         return new ApplicationRunner() {
             @Override
             public void run(ApplicationArguments args) throws Exception {
-                System.out.println("hihi");
+
+                if(postRepository.count() > 0) return;
+
+                Post p1 = new Post();
+                p1.setTitle("첫번째 게시글");
+                p1.setContent("첫번째 게시글 내용입니다.");
+
+                Post p2 = new Post();
+                p2.setTitle("두번째 게시글");
+                p2.setContent("두번째 게시글 내용입니다.");
+
+                Post p3 = new Post();
+                p3.setTitle("세번째 게시글");
+                p3.setContent("세번째 게시글 내용입니다.");
+
+                postRepository.save(p1);
+                postRepository.save(p2);
+                postRepository.save(p3);
             }
         };
 
