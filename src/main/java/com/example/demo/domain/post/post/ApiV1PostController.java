461ccd1d1e7b0f7a2359c9b3a7c9060beaa3359a
chacha86 <taejincha0619@gmail.com>
2025-11-28 19:51:31 +0900

    work

--- src/main/java/com/example/demo/domain/post/post/ApiV1PostController.java
+++ src/main/java/com/example/demo/domain/post/post/ApiV1PostController.java
@@ -3,6 +3,8 @@ package com.example.demo.domain.post.post;
 import com.example.demo.domain.member.Member;
 import com.example.demo.domain.member.MemberService;
 import com.example.demo.global.exception.ServiceException;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -17,12 +19,22 @@ public class ApiV1PostController {
     private final PostService postService;
     private final MemberService memberService;
 
+    @Getter
+    @NoArgsConstructor
+    static class WriteReqBody {
+        private String title;
+        private String content;
+        private String username;
+        private String password;
+    }
+
+
     @PostMapping("/api/v1/posts")
     @ResponseBody
-    public RsData write(String title, String content, String username, String password) {
+    public RsData write(@RequestBody WriteReqBody writeReqBody) {
 
         // 우리 회원이 맞는지 체크
-        Optional<Member> opMember = memberService.findByUsername(username);
+        Optional<Member> opMember = memberService.findByUsername(writeReqBody.username);
 
         if (opMember.isEmpty()) {
             throw new ServiceException("401", "없는 회원입니다.");
@@ -30,15 +42,15 @@ public class ApiV1PostController {
 
         Member member = opMember.get();
 
-        if (!member.getPassword().equals(password)) {
+        if (!member.getPassword().equals(writeReqBody.password)) {
             throw new ServiceException("401", "비밀번호를 틀렸습니다.");
         }
 
-        postService.write(title, content);
+        postService.write(writeReqBody.title, writeReqBody.content);
 
         RsData rsData = new RsData();
         rsData.setResultCode("200");
-        rsData.setMessage(username + "님의 글 작성이 완료되었습니다.");
+        rsData.setMessage(writeReqBody.username + "님의 글 작성이 완료되었습니다.");
 
         return rsData;
     }
